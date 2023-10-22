fb6f6784e502af175d3cdaf624dc86144c54b586
krydenk <krydenk@mail.ru>
2023-10-22 14:15:28 +0300

    Home Work #2
    Review fixes

--- hw2/src/main/java/ru/otus/hw/config/AppConfig.java
+++ hw2/src/main/java/ru/otus/hw/config/AppConfig.java
@@ -1,9 +1,9 @@
 package ru.otus.hw.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 public class AppConfig implements TestConfig, TestFileNameProvider {
 
     @Value("${test.rightAnswersCountToPass}")
