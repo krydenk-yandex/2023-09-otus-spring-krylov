4e9325270d179094c9de2f05c21d22545f71d892
krydenk <krydenk@mail.ru>
2024-03-24 20:24:24 +0300

    Diploma work: bookmarks in library

--- diploma_work/library/src/main/java/ru/otus/library/controllers/BookmarkController.java
+++ diploma_work/library/src/main/java/ru/otus/library/controllers/BookmarkController.java
@@ -1,11 +1,14 @@
 package ru.otus.library.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.otus.library.models.Bookmark;
 import ru.otus.library.models.Chapter;
 import ru.otus.library.services.BookmarkService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
